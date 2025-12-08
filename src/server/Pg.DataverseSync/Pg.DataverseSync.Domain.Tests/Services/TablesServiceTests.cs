fa1d71e7542dee72c8205c65a2c03d412aeb5d24
Piotr Gaszewski <piotr.gaszewski@onedynamics.pl>
2025-12-08 09:32:07 +0100

    Implement TablesService and add unit tests

--- src/server/Pg.DataverseSync/Pg.DataverseSync.Domain.Tests/Services/TablesServiceTests.cs
+++ src/server/Pg.DataverseSync/Pg.DataverseSync.Domain.Tests/Services/TablesServiceTests.cs
@@ -1,11 +1,63 @@
+using Moq;
+using Pg.DataverseSync.Domain.Dto;
+using Pg.DataverseSync.Domain.Repositories;
+using Pg.DataverseSync.Domain.Services;
 using Pg.DataverseSync.Domain.Tests.Core;
+using Pg.DataverseSync.Model;
+using System;
+using System.Collections.Generic;
+using Xunit;
 
 namespace Pg.DataverseSync.Domain.Tests.Services
 {
     public class TablesServiceTests : ServiceTestBase
     {
+        private List<Table> _allTables = new List<Table>
+        {
+            new Table { Name = "Test 1", SchemaName = "pg_test1" },
+            new Table { Name = "Test 2", SchemaName = "pg_test2" },
+            new Table { Name = "Test 3", SchemaName = "pg_test3" }
+        };
+        [Fact]
+        public void GetUnsynchronizedTables_NoSyncTable_ReturnsAllStandardTables()
+        {
+            // Arrange
+            var repo = new Mock<IRepository>(); 
+            repo.Setup(r => r.GetActiveSynchronizedTables()).Returns(
+                new List<pg_synctable>());
+            repo.Setup(r => r.GetStandardTablesFromMetadata()).Returns(_allTables);
 
-     
+            var tablesService = new TablesService(repo.Object, this.tracingService);
+            
+            // Act
+            var unsynchronizedTables = tablesService.GetUnsynchronizedTables();
+
+            // Assert
+            Assert.NotNull(unsynchronizedTables); 
+            Assert.Equal(_allTables.Count, unsynchronizedTables.Count);
+        }
+
+        [Fact]
+        public void GetUnsynchronizedTables_SyncTableAvailable_ReturnsFilteredStandardTables()
+        {
+            // Arrange
+            var expectedTablesCount = 2; 
+            var repo = new Mock<IRepository>();
+            repo.Setup(r => r.GetActiveSynchronizedTables()).Returns(
+                new List<pg_synctable>() { new pg_synctable { Id = Guid.NewGuid(), pg_name = "pg_test1" } });
+            repo.Setup(r => r.GetStandardTablesFromMetadata()).Returns(_allTables);
+
+            var tablesService = new TablesService(repo.Object, this.tracingService);
+
+            // Act
+            var unsynchronizedTables = tablesService.GetUnsynchronizedTables();
+
+            // Assert
+            Assert.NotNull(unsynchronizedTables);
+            Assert.Equal(expectedTablesCount, unsynchronizedTables.Count);
+        }
+
+
     }
 }
 
--- src/server/Pg.DataverseSync/Pg.DataverseSync.Domain/Services/TablesService.cs
+++ src/server/Pg.DataverseSync/Pg.DataverseSync.Domain/Services/TablesService.cs
@@ -1,8 +1,8 @@
 using Microsoft.Xrm.Sdk;
 using Pg.DataverseSync.Domain.Dto;
 using Pg.DataverseSync.Domain.Repositories;
-using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Pg.DataverseSync.Domain.Services
 {
@@ -14,12 +14,25 @@ namespace Pg.DataverseSync.Domain.Services
 
         public List<Table> GetUnsynchronizedTables()
         {
-            return new List<Table>
-            {
-                new Table { Name = "Account", SchemaName = "account" },
-                new Table { Name = "Contact", SchemaName = "contact" },
-                new Table { Name = "Lead", SchemaName = "lead" }
-            };
+
+            var syncTablesNames = repository.GetActiveSynchronizedTables()
+                .Select(st => st.pg_name)
+                .ToList();
+
+            var allStandardTables = repository.GetStandardTablesFromMetadata();
+
+            var unsynchronizedTables = allStandardTables
+                .Where(t => !syncTablesNames.Contains(t.SchemaName))
+                .ToList();
+
+            return unsynchronizedTables; 
+
+            //return new List<Table>
+            //{
+            //    new Table { Name = "Account", SchemaName = "account" },
+            //    new Table { Name = "Contact", SchemaName = "contact" },
+            //    new Table { Name = "Lead", SchemaName = "lead" }
+            //};
         }
     }
 }
