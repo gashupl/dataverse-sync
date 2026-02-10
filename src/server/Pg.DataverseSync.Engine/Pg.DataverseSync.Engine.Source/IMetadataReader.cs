cf655cf4e0c82dd6f6f87cbc95f0568e40385393
Piotr Gaszewski <piotr.gaszewski@onedynamics.pl>
2026-02-10 08:40:37 +0100

    Initial .NET 10 Azure Functions and Engine Source projects setup

---
+++ src/server/Pg.DataverseSync.Engine/Pg.DataverseSync.Engine.Source/IMetadataReader.cs
@@ -0,0 +1,11 @@
+﻿using Pg.DataverseSync.Engine.Source.Model;
+namespace Pg.DataverseSync.Engine.Source
+{
+    public interface IMetadataReader
+    {
+        List<Table>? GetTables();
+
+        List<Column> GetColumns(string tableName);
+
+    }
+}
