517c920d760dff8f57774afc89db4d4503c251ee
Piotr Gaszewski <piotr.gaszewski@onedynamics.pl>
2026-04-16 08:48:31 +0200

    Start SQL Docker container before API launch

--- src/server/Pg.DataverseSync.Api/Pg.DataverseSync.Api.AppHost/AppHost.cs
+++ src/server/Pg.DataverseSync.Api/Pg.DataverseSync.Api.AppHost/AppHost.cs
@@ -1,12 +1,17 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
 const int webAppPort = 5173;
+const string sqlContainerName = "local_sql_2025";
 
 var webAppWorkingDirectory = Path.GetFullPath(
     Path.Combine(builder.AppHostDirectory, "../../../client/Pg.DataverseSync.Web"));
 
 //See ADR-0002: docs/adr/0002-db-container-auto-generation-resign.md
 var database = builder.AddConnectionString("sqldb");
+
+//// If Docker is running, start the container (will do nothing if already started)
+var dockerStart = builder.AddExecutable("start-docker-container", "docker", builder.AppHostDirectory, "start", sqlContainerName)
+    .ExcludeFromManifest();
 
 var webApp = builder.AddExecutable("pg-dataversesync-web", "npm", webAppWorkingDirectory, "run", "dev")
     .WithHttpEndpoint(port: webAppPort, env: "PORT")
@@ -15,7 +20,8 @@ var webApp = builder.AddExecutable("pg-dataversesync-web", "npm", webAppWorkingD
 var api = builder.AddProject<Projects.Pg_DataverseSync_Api>("pg-dataversesync-api")
     .WithReference(database)
     .WithEnvironment("AllowedOrigins__0", $"http://localhost:{webAppPort.ToString()}")
-    .WithEnvironment("AllowedOrigins__1", $"https://localhost:{webAppPort.ToString()}"); // Support both HTTP and HTTPS
+    .WithEnvironment("AllowedOrigins__1", $"https://localhost:{webAppPort.ToString()}") // Support both HTTP and HTTPS
+    .WaitFor(dockerStart); // Wait for container to start
 
 webApp.WithReference(api)
     .WaitFor(api);
