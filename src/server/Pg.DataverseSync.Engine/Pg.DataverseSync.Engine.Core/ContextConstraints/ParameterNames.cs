74d0fe995ae8464378cbac180c0d26d894f65d5b
Piotr Gaszewski <piotr.gaszewski@onedynamics.pl>
2026-07-16 08:49:25 +0200

    Add MessageNames and ParameterNames static classes

---
+++ src/server/Pg.DataverseSync.Engine/Pg.DataverseSync.Engine.Core/ContextConstraints/ParameterNames.cs
@@ -0,0 +1,12 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Pg.DataverseSync.Engine.Core.ContextConstraints
+{
+    [ExcludeFromCodeCoverage]
+    public static class ParameterNames
+    {
+        public const string Target = "Target";
+        public const string PreImage = "PreImage";
+        public const string PostImage = "PostImage";
+    }
+}
