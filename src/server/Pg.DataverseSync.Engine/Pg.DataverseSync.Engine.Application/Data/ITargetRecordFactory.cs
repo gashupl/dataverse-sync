46374ef5e7c7c7c0851305895669c9f9e769a94a
Piotr Gaszewski <piotr.gaszewski@onedynamics.pl>
2026-09-22 08:44:10 +0200

    Add TargetRecordFactory and interface with unit tests

---
+++ src/server/Pg.DataverseSync.Engine/Pg.DataverseSync.Engine.Application/Data/ITargetRecordFactory.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Xrm.Sdk;
+using Pg.DataverseSync.Engine.Target;
+
+namespace Pg.DataverseSync.Engine.Application.Data
+{
+    public interface ITargetRecordFactory
+    {
+        TargetRecord CreateForInsert(Entity entity);
+        TargetRecord CreateForUpdate(Entity entity);
+        TargetRecord CreateForDelete(EntityReference entityReference);
+    }
+}
---
+++ src/server/Pg.DataverseSync.Engine/Pg.DataverseSync.Engine.Application/TargetRecordFactory.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Xrm.Sdk;
+using Pg.DataverseSync.Engine.Application.Data;
+using Pg.DataverseSync.Engine.Target;
+
+namespace Pg.DataverseSync.Engine.Application
+{
+    public class TargetRecordFactory : ITargetRecordFactory
+    {
+        public TargetRecord CreateForInsert(Entity entity)
+        {
+            ArgumentNullException.ThrowIfNull(entity);
+
+            var tableName = entity.LogicalName;
+            var record = new TargetRecord(tableName);
+
+            foreach (var attribute in entity.Attributes)
+            {
+                record.AddColumn(attribute.Key, attribute.Value);
+            }
+
+            return record;
+        }
+
+        public TargetRecord CreateForUpdate(Entity entity)
+        {
+            ArgumentNullException.ThrowIfNull(entity);
+
+            var tableName = entity.LogicalName;
+            var record = new TargetRecord(tableName);
+
+            foreach (var attribute in entity.Attributes)
+            {
+                bool isPrimaryKey = attribute.Key.Equals($"{tableName}id", StringComparison.OrdinalIgnoreCase); 
+                record.AddColumn(attribute.Key, attribute.Value, isPrimaryKey);
+            }
+
+            return record;
+        }
+
+        public TargetRecord CreateForDelete(EntityReference entityReference)
+        {
+            ArgumentNullException.ThrowIfNull(entityReference);
+
+            var tableName = entityReference.LogicalName;
+            var record = new TargetRecord(tableName);
+
+            record.AddColumn($"{tableName}id", entityReference.Id, isPrimaryKey: true);
+
+            return record;
+        }
+
+    }
+}
